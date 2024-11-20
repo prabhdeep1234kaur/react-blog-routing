1870d53a51d92657d918320624f6ce7ee682d2ff
prabhdeep kaur <prabhdeep1234kaur@gmail.com>
2024-11-21 00:15:10 +0530

    Intro to routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+//header, nav and footer they stay on the page
+import Header from './Header';
+import Nav from './Nav';
+import Footer from './Footer';
+//elements that will change
+import Home  from './Home';
+import NewPost from './NewPost';
+import PostPage from './PostPage';
+import About from './About';
+import Missing from './Missing';
+
+import { Route, Routes, useHistory } from "react-router-dom";
+import {useState, useEffect} from 'react'; //hooks
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <Nav />
+      <Routes>
+        {/* Correct route definitions */}
+        <Route exact path="/" element={<Home />} />
+        <Route path="/post" element={<NewPost />} />
+        <Route path="/post/:id" element={<PostPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Missing />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/NewPost.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const NewPost = () => {
+  return (
+    <main>
+        <h2>New post</h2>
+    </main>
+  )
+}
+
+export default NewPost
